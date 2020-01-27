edecbc19f7c0d66a00a64e77f4e4c64f4bb4c8e1
ctkremer <colin.kremer@yale.edu>
2020-01-27 14:28:41 -0500

    adding general package documentation, including imports of S4 methods/classes from bbmle

---
+++ R/mleTools.R
@@ -0,0 +1,14 @@
+#' mleTools: A package for extending maximum likelihood analysis tools (in bbmle).
+#'
+#' The mleTools package does X, Y, and Z. (see: http://r-pkgs.had.co.nz/man.html#man-packages)
+#' 
+#' @section grid.mle2 functions:
+#' The grid.mle2 functions does ...
+#'
+#' @docType package
+#' @name mleTools
+#' 
+#' @import methods
+#' @importClassesFrom bbmle mle2
+#' @importMethodsFrom bbmle coef predict vcov logLik
+NULL
